16c06af679d00287c1a0e15f7e37c1cf48e7add0
jnicodev <im@jnico.dev>
2026-03-11 11:46:57 -0500

    build: when the user sign in a new session record is created

--- app/api/auth/signin/route.ts
+++ app/api/auth/signin/route.ts
@@ -1,22 +1,36 @@
-import { readFile } from 'node:fs/promises';
+import { NextResponse } from 'next/server';
+import { readFile, writeFile } from 'node:fs/promises';
 import path from 'node:path';
 
+import { findUser } from '@/lib/users';
 import { User } from '@/types/user';
 
 export async function POST(request: Request) {
-    const signIn: User = await request.json();
-
-    const filePath = path.join(process.cwd(), 'data/users.json');
-    const file = await readFile(filePath, 'utf-8');
-    const users: User[] = JSON.parse(file);
-
-    const user: undefined | User = users.find(user => user.email === signIn.email && user.password === signIn.password);
+    const { nickname }: User = await request.json();
+    const user = await findUser(nickname);
 
     if (!user) {
-        return Response.json({
-            'message': 'Correo electrónico o contraseña incorrecta',
+        return NextResponse.json({
+            message: 'Usuario o clave incorrecta',
+        }, {
+            status: 400,
         });
     }
 
-    return Response.json(user);
+    const token = crypto.getRandomValues(new Uint32Array(10)).toString();
+
+    const file = path.join(process.cwd(), 'data/sessions.json');
+    const data = await readFile(file, 'utf-8');
+    const sessions = JSON.parse(data);
+
+    const session ={
+        token,
+        userId: user.id,
+    };
+
+    sessions.push(session);
+
+    await writeFile(file, JSON.stringify(sessions, null, 2));
+
+    return NextResponse.json(session);
 }
