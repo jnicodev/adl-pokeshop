48b42ea4807903e05fb8cbf4a00ecb1467c55327
jnicodev <im@jnico.dev>
2026-03-12 12:00:31 -0500

    feat: user can logout using the menu button

--- components/molecules/MainMenu.tsx
+++ components/molecules/MainMenu.tsx
@@ -1,10 +1,37 @@
 'use client';
 
+import { useMutation } from '@tanstack/react-query';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 import Button from '@/components/atoms/Button/Button';
 
 const MainMenu = () => {
+    const router = useRouter();
+
+    // MUTATIONS
+    const signOut = useMutation({
+        mutationFn: async () => {
+            const response = await fetch('/api/auth/signout', {
+                method: 'POST',
+            });
+            const json = await response.json();
+
+            if (!response.ok) throw new Error(json.message);
+
+            return json;
+        },
+    });
+    const handleSignOut = async () => {
+        try {
+            await signOut.mutateAsync();
+
+            router.push('/');
+        } catch (error: unknown) {
+            alert(error);
+        }
+    };
+
     return (
         <nav className='flex items-center justify-between bg-neutral-900 py-3 px-5 border-6 border-rose-800'>
             <Link
@@ -41,7 +68,7 @@ const MainMenu = () => {
                 </div>
             </div>
 
-            <Button>
+            <Button onPress={ handleSignOut }>
                 Salir
             </Button>
         </nav>
