0e4604a89655c7cb5f9b68379c5a898551cbaf25
jnicodev <im@jnico.dev>
2026-03-12 21:35:17 -0500

    style: items count color changed

--- components/molecules/MainMenu.tsx
+++ components/molecules/MainMenu.tsx
@@ -69,7 +69,7 @@ const MainMenu = () => {
                         width={ 70 }
                     />
 
-                    <span className='text-xs leading-1.5 bg-mauve-500 p-2 border border-mauve-400 rounded-full absolute -top-2 -right-2'>
+                    <span className='text-xs leading-1.5 bg-olive-500 p-2 border border-olive-400 rounded-full absolute -top-2 -right-2'>
                         { cart.total.items }
                     </span>
                 </Button>
